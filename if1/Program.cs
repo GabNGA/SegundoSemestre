d8934cf5007f9c47301173dcf31d6ea66ce51d14
GabNGA <ganarvaez@uamv.edu.ni>
2025-08-29 09:57:05 -0600

    agregue Af1

--- if1/Program.cs
+++ if1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double ancho = 5.0;
+        const double varaspormanzanas = 1428.57;
+        Console.WriteLine("ingrese la longitud del terreno en varas: ");
+        double longitud = Convert.ToDouble(Console.ReadLine());
+        double area = ancho * longitud;
+        double manzanas = area / varaspormanzanas;
+        Console.WriteLine($"El area del terreno es: {area}  varas cuadradas");
+        Console.WriteLine($"El area del terreno en manzanas es: {manzanas}");
+
+    }
+}
+   
--- if1/obj/Debug/net8.0/if1.AssemblyInfo.cs
+++ if1/obj/Debug/net8.0/if1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("if1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f432901f708404713de04e21b29a6bf44492266c")]
 [assembly: System.Reflection.AssemblyProductAttribute("if1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("if1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
