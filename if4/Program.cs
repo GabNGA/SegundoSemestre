d413354a9552f275b165f8ff5dc2841209b290ca
GabNGA <ganarvaez@uamv.edu.ni>
2025-08-29 13:51:52 -0600

    cuarto ejercicio agregado

---
+++ if4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Ingrese el largo del tanque: ");
+        double largo = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el ancho del tanque: ");
+        double ancho = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la altura del tanque: ");
+        double altura = double.Parse(Console.ReadLine());
+        double volumen = (altura * ancho * largo) / 1000;
+        Console.WriteLine($"El volumen del tanque es: {volumen} metros cúbicos");   
+        
+    }
+}
