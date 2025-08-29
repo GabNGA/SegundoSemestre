7f7adc41e57e6b6b4fe8c6219482bdc98b978548
GabNGA <ganarvaez@uamv.edu.ni>
2025-08-29 10:18:31 -0600

    Subi el segundo ejercico

--- if2/Program.cs
+++ if2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Desarrolle un programa que calcule el tiempo total en horas de un viaje en bus desde Managua a Granada, donde el usuario ingresa la velocidad promedio en kilómetros por hora como entrada, asumiendo una distancia fija de 40 kilómetros.
+using System;
+
+class Program
+{  
+    static void Main()
+    {  
+        const double distancia = 40.0;
+        Console.WriteLine("Ingrese la velocidad promedio en km/h: ");
+        double velocidad = Convert.ToDouble(Console.ReadLine());
+        double tiempo = distancia / velocidad;
+        Console.WriteLine($"El tiempo total del viaje es: {tiempo} horas");
+    }
+} 
+
